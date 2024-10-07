4c16ca416758e23cc05e32849b55bac35a5d113b
Krishna-Ningaraddi <krishna.ningaraddi08@gmail.com>
2024-10-07 21:53:49 +0530

    Day2 Commit

--- Basics/Day1.js
+++ Basics/Day1.js
@@ -1,2 +1,13 @@
-console.log("hello world")
-console.log("Hello Krishna")
+// Day 1 Variables and constants
+
+const accountId = 24494
+let accountEmail="kningaraddi@lenovo.com"
+var accountPassword="AQcv-8587"
+accountCity= "Bangalore"
+
+// accountId=1
+
+console.log(accountId);
+console.log(accountEmail);
+console.log(accountCity);
+
---
+++ Basics/Day2.js
@@ -0,0 +1,17 @@
+//Variables
+
+let firstName= "krishna"
+let lastName= "Ningaraddi"
+
+
+let car1={name:"volvo",model:"A4",price:450000}
+let car2={name:"BMW",model:"M5",price:400000}
+let car3={name:"Audi",model:"Q7",price:750000}
+let car4={name:"mercedes",model:"sClass",price:850000}
+
+console.table([car1,car2,car3,car4])
+
+
+
+
+ 
